9076d31126f9ad5b789687bd360ba8acf3402531
Beny DZIENGUE <patdenzoungany@gmail.com>
2024-04-24 14:22:59 +0100

    cqroussel

--- components/caroussel.tsx
+++ components/caroussel.tsx
@@ -45,6 +45,13 @@ const Caroussel = () => {
           className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
           aria-label="Slide 3"
         ></button>
+        <button
+          type="button"
+          data-twe-target="#carouselExampleCaptions"
+          data-twe-slide-to="3"
+          className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+          aria-label="Slide 4"
+        ></button>
       </div>
 
       {/* <!--Carousel items--> */}
@@ -56,11 +63,7 @@ const Caroussel = () => {
           data-twe-carousel-item
           style={{ backfaceVisibility: "hidden" }}
         >
-          <img
-            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(15).jpg"
-            className="block w-full"
-            alt="..."
-          />
+          <div className="block h-[90vh] bg-[url('/Images/hero1.png')] bg-cover bg-no-repeat"></div>
           <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
             <h5 className="text-xl">First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
@@ -72,11 +75,7 @@ const Caroussel = () => {
           data-twe-carousel-item
           style={{ backfaceVisibility: "hidden" }}
         >
-          <img
-            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(22).jpg"
-            className="block w-full"
-            alt="..."
-          />
+          <div className="block w-full h-[90vh] bg-[url('/Images/hero2.png')] bg-cover bg-no-repeat"></div>
           <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
             <h5 className="text-xl">Second slide label</h5>
             <p>Some representative placeholder content for the second slide.</p>
@@ -88,13 +87,21 @@ const Caroussel = () => {
           data-twe-carousel-item
           style={{ backfaceVisibility: "hidden" }}
         >
-          <img
-            src="https://tecdn.b-cdn.net/img/Photos/Slides/img%20(23).jpg"
-            className="block w-full"
-            alt="..."
-          />
+          <div className="block w-full h-[90vh] bg-[url('/Images/hero3.png')] bg-cover bg-no-repeat"></div>
           <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
             <h5 className="text-xl">Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+        {/* <!--Fourth item--> */}
+        <div
+          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+          data-twe-carousel-item
+          style={{ backfaceVisibility: "hidden" }}
+        >
+          <div className="block w-full h-[90vh] bg-[url('/Images/hero4.png')] bg-cover bg-no-repeat"></div>
+          <div className="absolute inset-x-[15%] bottom-5 hidden py-5 text-center text-white md:block">
+            <h5 className="text-xl">Fourth slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
           </div>
         </div>
